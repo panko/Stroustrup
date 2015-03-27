46162084ae9f0ee0a97a27c87af90401b607e2a1
prike <prike18@gmail.com>
2015-03-27 10:48:24 +0100

    4.11.one two redy

---
+++ 4.11.Gyakorlatok/1.cpp
@@ -0,0 +1,10 @@
+/*
+1. (*2) Futtassuk le a Helló, világ! programot (§3.2). Ha a program fordítása nem
+úgy sikerül, ahogy kellene, olvassuk el §B.3.1-et.
+*/
+#include <iostream>
+
+int main(){
+	std::cout << "Helló, világ!" << std::endl;
+	return 0;
+}
---
+++ 4.11.Gyakorlatok/2.cpp
@@ -0,0 +1,77 @@
+/*
+2. (*1) §4.9 minden deklarációjára végezzük el a következõket: ha a deklaráció
+nem definíció, írjunk hozzá definíciót. Ha a deklaráció definíció is, írjunk olyan
+deklarációt, ami nem az.
+*/
+#include <string>
+#include <complex>
+
+struct Date { int d, m, y; };
+
+int day(Date* p) { return p->d; }
+template<class T> T abs(T a) { return a<0 ? -a : a; }
+
+double sqrt(double);
+
+void f()
+{
+int count = 1;
+char* name = "Bjarne";
+// ...
+count = 2;
+name = "Marian";
+}
+
+namespace NS { int a; }
+
+int main(){
+
+const char* ch;
+ch = "a";
+
+std::string s;
+s = "asd";
+
+int count = 1;
+const double pi = 3.1415926535897932385;
+
+
+char* name = "Natasa";
+char* season[ ] = { "tavasz", "nyár", "õsz", "tél" };
+
+struct Date { int d, m, y; };
+
+
+
+
+
+
+typedef std::complex<short> Point;
+struct User;
+enum Beer { Carlsberg, Tuborg, Thor };
+
+
+
+
+
+struct User;
+
+
+
+struct User { /* ... */ };
+
+
+
+extern int error_number;
+
+
+
+
+
+
+
+
+
+
+}
+// this is a fckin useless exericise i dont even know why im doin this
